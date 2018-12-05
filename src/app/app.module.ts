a29685ab0b97ef1e519b03e7f034db7caf9c69ef
Paul Goroshko <mcredpower@gmail.com>
2018-12-05 15:19:21 +0300

    add project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { UserInfoPageComponent } from './user-info-page/user-info-page.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'user',
+    component: UserInfoPageComponent,
+  },
+  {
+  path: '**',
+  component: LoginPageComponent,
+  }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    UserInfoPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
